74e0fbb58e6997dff59388f2a5857f790df957b7
EnerGumen <fredolecoco@hotmail.fr>
2015-05-10 15:20:50 +0200

    srand dans les fonctions c'est le mal ;) Algo génétique fini dsl florian ^^

--- main.c
+++ main.c
@@ -1,39 +1,38 @@
 #include "openshop.h"
 
-int main(){    
-    nb_jobs = 4;
-    nb_taches = 9;
+int main(){  
+	srand(time(NULL));
+	nb_jobs = 4;
+	nb_taches = 9;
 	nb_pop=100;
-Tache taches[9];
-    taches[0] = nouvelle_tache(1,1,6);
-    taches[1] = nouvelle_tache(3,1,2);
-    taches[2] = nouvelle_tache(2,2,1);
-    taches[3] = nouvelle_tache(3,2,5);
-    taches[4] = nouvelle_tache(1,2,3);
-    taches[5] = nouvelle_tache(1,3,4);
-    taches[6] = nouvelle_tache(1,4,7);
-    taches[7] = nouvelle_tache(2,4,1);
-    taches[8] = nouvelle_tache(3,4,8);
 
+	nb_pop_keep=5;
+	nb_time=100;
+	Tache taches[9];
+	taches[0] = nouvelle_tache(1,1,6);
+	taches[1] = nouvelle_tache(3,1,2);
+	taches[2] = nouvelle_tache(2,2,1);
+	taches[3] = nouvelle_tache(3,2,5);
+	taches[4] = nouvelle_tache(1,2,3);
+	taches[5] = nouvelle_tache(1,3,4);
+	taches[6] = nouvelle_tache(1,4,7);
+	taches[7] = nouvelle_tache(2,4,1);
+	taches[8] = nouvelle_tache(3,4,8);
 
+	temp = nouvelle_pop(taches);
 	jobs = nouvelle_pop(taches);
 	next_gen = nouvelle_pop(taches);
-trouve_solution_croissante(0);
-affichage_machine_ASCII(0);
+
+
+
+algo_genetique();
+printf("Somme : %d\n",calcul_obj(0));
 printf("\n");
-trouve_solution_decroissante(0);
 affichage_machine_ASCII(0);
-printf("\n");
-heuristique_bizarre(0);
-affichage_machine_ASCII(0);
-printf("\n");
-aleatoire(0);
-affichage_machine_ASCII(0);
-printf("\n");
 
 
 //croisement(taches,jobs[0],jobs[1]);
-mutation(taches, jobs[0]);
+//mutation(taches, jobs[0]);
     return 0;
 
 }
--- openshop.c
+++ openshop.c
@@ -83,6 +83,46 @@ void affichage_machine_ASCII(int indice){
 	}
 }
 
+void affiche_next(int indice){
+    	int i,j;
+    	for (i=0;i<nb_jobs;i++) {
+        	printf("Job n°%d\n",i+1);
+        	for (j=0;j<3;j++)
+			if (next_gen[indice][i]->taches[j].job!=-1)
+		        	printf("Machine: %d, debut: %d, duree: %d\n",next_gen[indice][i]->taches[j].machine,next_gen[indice][i]->taches[j].debut,next_gen[indice][i]->taches[j].duree);
+    	}
+}
+
+void affiche_machine_next(int indice){
+	int i,j;
+	for (i=0;i<3;i++){
+		printf("Machine n°%d\n",i+1);
+		for (j=0;j<nb_jobs;j++)
+			if (next_gen[indice][j]->taches[i].job!=-1)
+				printf("Job: %d, debut: %d, duree: %d\n",next_gen[indice][j]->taches[i].job,next_gen[indice][j]->taches[i].debut,next_gen[indice][j]->taches[i].duree);
+	}
+}
+
+void affichage_machine_ASCII_next(int indice){
+	int i,j,current,k;
+	for (i=0;i<3;i++){
+		printf("Machine n°%d\t",i+1);
+		for (j=0;j<date_fin(indice);j++){
+			current=0;
+			for (k=0;k<nb_jobs;k++)
+				if (next_gen[indice][k]->taches[i].debut!=-1 && next_gen[indice][k]->taches[i].debut<=j && next_gen[indice][k]->taches[i].debut+next_gen[indice][k]->taches[i].duree>j){
+					current=next_gen[indice][k]->taches[i].job;
+					break;
+				}
+			if (current==0)
+				printf("-");
+			else
+				printf("%d",current);
+		}
+		printf("\n");		
+	}
+}
+
 void affichage_solution_ASCII(Job** jobs){
 	int i,j,current,k;
 	for (i=0;i<3;i++){
@@ -106,7 +146,7 @@ void affichage_solution_ASCII(Job** jobs){
 Tache* tri_croissant(int indice){
 
     int i = nb_taches,j,k,pos;
-    Tache *taches = malloc(nb_taches*sizeof(Tache*));
+    Tache *taches = malloc(nb_taches*sizeof(Tache));
     pos = 0;
     for (k = 0 ; k < nb_jobs ; k++)
         for (j = 0 ; j < 3 ; j++)
@@ -135,7 +175,7 @@ Tache* tri_croissant(int indice){
 
 Tache* tri_decroissant(int indice){
     int i = nb_taches,j,k,pos;
-    Tache *taches = malloc(nb_taches*sizeof(Tache*));
+    Tache *taches = malloc(nb_taches*sizeof(Tache));
     pos = 0;
     for (k = 0 ; k < nb_jobs ; k++)
         for (j = 0 ; j < 3 ; j++)
@@ -167,6 +207,13 @@ void raz_debut_jobs(int indice){
     	for (i=0;i<nb_jobs;i++)
         	for (j=0;j<3;j++)
 			jobs[indice][i]->taches[j].debut=-1;
+}
+
+void raz_debut_next(int indice){
+	int i,j;
+    	for (i=0;i<nb_jobs;i++)
+        	for (j=0;j<3;j++)
+			next_gen[indice][i]->taches[j].debut=-1;
 }
 
 void trouve_solution_croissante(int indice){
@@ -250,6 +297,69 @@ void put_tache(Tache t, int indice){
 
 }
 
+void put_tache_next(Tache t, int indice){
+	int i;
+	int j=0;
+	int bornes [nb_jobs+1][2];
+	int start = 0;
+	int fin = start+t.duree;
+	int boolean=1;
+
+	//Récupère les bornes pour le même job
+	for (i=0;i<3;i++)
+		if (i!=t.machine-1){
+			if (next_gen[indice][t.job-1]->taches[i].debut!=-1){
+				bornes[j][0]=next_gen[indice][t.job-1]->taches[i].debut;
+				bornes[j][1]=next_gen[indice][t.job-1]->taches[i].debut+next_gen[indice][t.job-1]->taches[i].duree;
+			}
+			else{
+				bornes[j][0]=-1;
+				bornes[j][1]=-1;
+			}
+			j++;
+		}
+	//Récupère les bornes pour la même machines
+	for (i=0;i<nb_jobs;i++)	
+		if (i!=t.job-1){
+			if (next_gen[indice][i]->taches[t.machine-1].debut!=-1){
+				bornes[j][0]=next_gen[indice][i]->taches[t.machine-1].debut;
+				bornes[j][1]=next_gen[indice][i]->taches[t.machine-1].debut+next_gen[indice][i]->taches[t.machine-1].duree;
+			}
+			else{
+				bornes[j][0]=-1;
+				bornes[j][1]=-1;
+			}
+			j++;
+		}
+	//Test si possible de mettre à 0
+	for (i=0;i<nb_jobs+1;i++)
+		if (bornes[i][0]!=-1 && bornes[i][0]<fin){
+			boolean=0;
+			break;
+		}
+
+	//Si pas possible cherche le minimum où mettre
+	if (boolean==0){
+		start=INT_MAX;
+		for (i=0;i<nb_jobs+1;i++)
+			if (bornes[i][0]!=-1){
+				boolean=1;
+				for (j=0;j<nb_jobs+1;j++)
+					if (bornes[j][0]!=-1 && bornes[j][0]<(bornes[i][1]+t.duree) && bornes[j][1]>bornes[i][1]){
+						boolean=0;
+						break;
+					}
+				if (boolean==1){
+					start=min(start,bornes[i][1]);
+					fin=start+t.duree;
+				}
+			}
+	}
+	
+	next_gen[indice][t.job-1]->taches[t.machine-1].debut=start;
+
+}
+
 void heuristique_bizarre(int indice){
 	Tache *t = tri_bizarre(indice);
     	int i;
@@ -266,8 +376,7 @@ void aleatoire(int indice){
 	int i;
 	int alea;
 	int test[nb_taches];
-	raz_debut_jobs(indice);
-	srand(time(NULL));		
+	raz_debut_jobs(indice);		
 	for (i=0;i<nb_taches;i++)
 		test[i]=0;
 	i=0;
@@ -325,7 +434,7 @@ int tache_max(int job, int place, int indice){
 }
 
 Tache * tri_bizarre(int indice){
-	Tache *taches = malloc(nb_taches*sizeof(Tache*));
+	Tache *taches = malloc(nb_taches*sizeof(Tache));
 	int duree_jobs[nb_jobs];
 	int i,j;
 	int current_job,current_machine;	
@@ -374,8 +483,7 @@ int min(int a, int b){
     return a > b ? b : a;
 }
 
-Job** croisement(Tache *taches, Job** j1, Job** j2){ //Ne test pas la validité de la solution !!
-	srand(time(NULL));
+/*Job** croisement(Tache *taches, Job** j1, Job** j2){ //Ne test pas la validité de la solution !!
 	int fils = rand()%2; //parent sur lequel on coupe
 	int m = rand()%3; //machine sur lequel on fait le croisement
 	int ptCoupe = 0;
@@ -419,9 +527,194 @@ Job** croisement(Tache *taches, Job** j1, Job** j2){ //Ne test pas la validité 
 	affichage_solution_ASCII(j2);
 	affichage_solution_ASCII(res);
 	return res;
+}*/
+
+
+void croisement (int p1, int p2, int enf){
+	int parent = rand()%2; 	//parent sur lequel on coupe
+	if (parent==1){
+		parent=p1;
+		p1=p2;
+		p2=parent;
+	}
+	int m = rand()%3;	//machine sur laquelle on coupe
+	int ptCoupe = rand()%nb_jobs;
+	ptCoupe=jobs[p1][ptCoupe]->taches[m].debut+jobs[p1][ptCoupe]->taches[m].duree;
+	Tache *t = malloc(nb_jobs*sizeof(Tache));
+	int i,j,k=0;	
+
+	for (i=0;i<nb_jobs;i++)
+		for (j=0;j<3;j++)
+			if (j!=m)
+				next_gen[enf][i]->taches[j]=jobs[p1][i]->taches[j];
+			else
+				if (jobs[p1][i]->taches[j].debut+jobs[p1][i]->taches[j].duree<=ptCoupe)
+					next_gen[enf][i]->taches[j]=jobs[p1][i]->taches[j];
+				else{
+					t[k]=jobs[p2][i]->taches[j];
+					k++;
+				}
+
+	
+
+
+	int echange = 1;
+	Tache tmp;
+	i = k;
+	while ((i>0) && (echange)){
+		echange = 0;
+		for (j = 0 ; j < i-1 ; j++){
+			if (t[j].debut > t[j+1].debut){
+		        	tmp = t[j];
+		        	t[j] = t[j+1];
+		        	t[j+1] = tmp;
+		        	echange = 1;
+		    	}
+		}
+		i--;
+    	}
+	
+
+	for (i=0;i<k;i++)
+		put_tache_next(t[i],enf);
+			
+
 }
 
-Job** mutation(Tache* taches, Job** jobs){
+void mutation(int p, int enf){
+	int m = rand()%3;	//machine sur laquelle on coupe
+	int i,j;
+
+	for (i=0;i<nb_jobs;i++)
+		for (j=0;j<3;j++)
+			if (j!=m)
+				next_gen[enf][i]->taches[j]=jobs[p][i]->taches[j];
+
+	j=rand()%nb_jobs;
+
+	for (i=0;i<nb_jobs;i++){
+		if (jobs[p][j]->taches[m].duree!=-1)
+			put_tache_next(jobs[p][j]->taches[m],enf);
+		j=(j+1)%nb_jobs;
+	}				
+				
+
+
+}
+
+int calcul_obj(int indice){
+	int i,j,maxc,somme=0;
+
+	for (i=0;i<nb_jobs;i++){
+		maxc=0;
+		for (j=0;j<3;j++)
+			maxc = max(maxc,jobs[indice][i]->taches[j].debut+jobs[indice][i]->taches[j].duree);
+		somme +=  maxc;
+	}
+
+	return somme;
+		
+
+}
+
+void tri_pop(){
+	int i,j,tmp;
+	int indice[nb_pop];
+	int somme[nb_pop];
+	
+	for (i=0;i<nb_pop;i++){
+		somme[i]=calcul_obj(i);
+		indice[i]=i;
+		temp[i]=jobs[i];
+	}
+	int echange=1;
+	i=nb_pop;
+	while ((i>0) && (echange)){
+		echange = 0;
+		for (j = 0 ; j < i-1 ; j++){
+			if (somme[j] > somme[j+1]){
+		        	tmp = somme[j];
+		        	somme[j] = somme[j+1];
+		        	somme[j+1] = tmp;
+				tmp = indice[j];
+				indice[j] = indice[j+1];
+				indice[j+1] = tmp;
+				echange = 1;
+		    	}
+		}
+		i--;
+    	}
+
+	for (i=0;i<nb_pop;i++)
+		jobs[i]=temp[indice[i]];
+
+}
+
+void algo_genetique(){
+	int i,alea,p1,p2,j=0;
+
+	//Création de la population de base
+	for (i=0;i<nb_pop;i++){
+		raz_debut_jobs(i);		
+		raz_debut_next(i);
+		aleatoire(i);
+	}
+	
+	while(j!=nb_time){
+		//Tri de la population
+		tri_pop();
+		
+		//affichage_machine_ASCII(0);
+		for (i=0;i<nb_pop;i++)
+			if (i<nb_pop_keep)
+				next_gen[i]=jobs[i];
+			else{	
+				alea=rand()%100;
+				if (alea<80){
+					p1=rand()%100;
+					if (p1<50)
+						p1=rand()%(int)(0.2*nb_pop);
+					else if (p1>80)
+						p1=rand()%(int)(0.3*nb_pop)+0.7*nb_pop;
+					else
+						p1=rand()%(int)(0.5*nb_pop)+0.2*nb_pop;
+					p2=rand()%100;
+					if (p2<50)
+						p2=rand()%(int)(0.2*nb_pop);
+					else if (p2>80)
+						p2=rand()%(int)(0.3*nb_pop)+0.7*nb_pop;
+					else
+						p2=rand()%(int)(0.5*nb_pop)+0.2*nb_pop;
+					croisement(p1,p2,i);
+					
+				}
+				else if (alea>95){
+					next_gen[i]=jobs[i];
+				}
+				else{
+					p1=rand()%100;
+					if (p1<50)
+						p1=rand()%(int)(0.5*nb_pop);
+					else if (p1>80)
+						p1=rand()%(int)(0.3*nb_pop)+0.7*nb_pop;
+					else
+						p1=rand()%(int)(0.2*nb_pop)+0.2*nb_pop;
+					mutation(p1,i);
+				}
+
+			}
+		for (i=0;i<nb_pop;i++)
+			jobs[i]=next_gen[i];
+		
+		j++;
+	}
+	tri_pop();
+	
+			
+
+}
+
+/*Job** mutation(Tache* taches, Job** jobs){
 	printf("mutation\n");
 	affichage_solution_ASCII(jobs);
 	
@@ -514,4 +807,4 @@ Job** mutation(Tache* taches, Job** jobs){
 	printf("ap mut\n");
 	affichage_solution_ASCII(res);
 	return res;
-}
+}*/
